8918b4d2472da177b80b9dd4c39f08b2e6c52ab7
dinamohammed <dina.mohammed392@gmail.com>
2021-03-03 17:21:20 +0000

    Add Multi Transfers in PO

--- weight_bridge/models/.ipynb_checkpoints/stock_picking_cus-checkpoint.py
+++ weight_bridge/models/.ipynb_checkpoints/stock_picking_cus-checkpoint.py
@@ -4,6 +4,7 @@ from odoo import models, fields, api, _
 from datetime import datetime
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 from odoo.exceptions import UserError , ValidationError
+from calendar import monthrange
             
 
 class StockPicking(models.Model):
@@ -17,13 +18,36 @@ class SaleOrder(models.Model):
     
     
     period_month = fields.Boolean('Divide Transfers')
-    no_of_transfers = fields.Integer('Transfers per month', default = fields.Date.today().day)
+    no_of_transfers = fields.Integer('Transfers per month', 
+                                     default = monthrange(fields.Date.today().year,fields.Date.today().month)[1])
     
     
     def multi_transfers(self):
         product_quantity = self.order_line[0].product_uom_qty
         quantity_divide = int(product_quantity / self.no_of_transfers)
-        for _ in range(self.no_of_transfers):
+        for _ in range(self.no_of_transfers-1):
+            self.picking_ids[0].copy()
+        for pick in self.picking_ids:
+            pick.write({'state':'waiting'})
+            for move in pick.move_ids_without_package:
+                move.write({'product_uom_qty':quantity_divide,
+                           'state':'waiting'})
+                
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+    
+    
+    period_month = fields.Boolean('Divide Transfers')
+    no_of_transfers = fields.Integer('Transfers per month',
+                                     default = monthrange(fields.Date.today().year,fields.Date.today().month)[1])
+    
+    
+    
+    def multi_transfers(self):
+        product_quantity = self.order_line[0].product_qty
+        quantity_divide = int(product_quantity / self.no_of_transfers)
+        for _ in range(self.no_of_transfers-1):
             self.picking_ids[0].copy()
         for pick in self.picking_ids:
             pick.write({'state':'waiting'})
